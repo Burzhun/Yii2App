d823dad993617b08bae7acaace50069417cd7a01
burjunov <burjunov@yandex.ru>
2018-02-02 14:14:14 +0300

    Yii app update

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -61,7 +61,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect(['users/index']);
+        //return $this->render('index');
     }
 
     /**
